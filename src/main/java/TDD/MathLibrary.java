0692dd483faebcb8ac40f45d0f8e8781ccae0bed
Arturo Rubio <arubio@seocom.agency>
2024-02-09 15:45:14 +0100

    test sumList method and add @BeforeEach & @DisplayName annotations

--- src/main/java/TDD/MathLibrary.java
+++ src/main/java/TDD/MathLibrary.java
@@ -1,7 +1,18 @@
 package TDD;
 
+import java.util.List;
+
 public class MathLibrary {
     public int add(int x, int y) {
         return x + y;
+    }
+
+    public int sumList(List<Integer> arr) {
+        int sum = 0;
+
+        for (int item : arr) {
+            sum += item;
+        }
+        return sum;
     }
 }
--- src/test/java/TDD/MathLibraryTest.java
+++ src/test/java/TDD/MathLibraryTest.java
@@ -1,21 +1,52 @@
 package TDD;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MathLibraryTest {
+    private MathLibrary math;
+
+    @BeforeEach
+    public void setUp() {
+        math = new MathLibrary();
+    }
 
     @Test
+    @DisplayName("Sum 2 positive ints with add() method")
     public void add_twoPositiveInts_Summed() {
-        MathLibrary math = new MathLibrary();
         assertEquals(2, math.add(1, 1));
         assertEquals(18, math.add(3, 15));
     }
 
     @Test
+    @DisplayName("Sum 2 negatives ints with add() method")
     public void add_twoNegativeInts_Summed() {
-        MathLibrary math = new MathLibrary();
         assertEquals(-2, math.add(-1, -1));
         assertEquals(-18, math.add(-3, -15));
+    }
+
+    @Test
+    @DisplayName("Sum a List of positive ints with sumList() method")
+    void sumList_arrListPositiveInts_Summed() {
+        assertEquals(5, math.sumList(Arrays.asList(1,1,1,1,1)));
+        assertEquals(15, math.sumList(Arrays.asList(1,2,3,4,5)));
+    }
+
+    @Test
+    @DisplayName("Sum a List of negative ints with sumList() method")
+    void sumList_arrListNegativeInts_Summed() {
+        assertEquals(-5, math.sumList(Arrays.asList(-1,-1,-1,-1,-1)));
+        assertEquals(-15, math.sumList(Arrays.asList(-1,-2,-3,-4,-5)));
+    }
+    @Test
+    @DisplayName("Sum a List of positive & negative ints with sumList() method")
+    void sumList_arrListPositiveAndNegativeInts_Summed() {
+        assertEquals(1, math.sumList(Arrays.asList(1,-1,1,-1,1)));
+        assertEquals(-5, math.sumList(Arrays.asList(-10,2,3,5,-5)));
     }
 }

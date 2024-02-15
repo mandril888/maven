8d80ad111fc8363b8849e4d95296537ddc204c10
mandril888 <arturo.rubio.89.8@gmail.com>
2024-02-15 20:27:35 +0100

    enums

---
+++ src/main/java/Enums/Account.java
@@ -0,0 +1,11 @@
+package Enums;
+
+public class Account {
+    private String name;
+    private String address;
+    private double balance;
+    private String accountNumber;
+    private Hold hold;
+
+    // constructor, getters and setters
+}
---
+++ src/main/java/Enums/Hold.java
@@ -0,0 +1,5 @@
+package Enums;
+
+public enum Hold {
+    NONE, FRAUD, DELINQUENCY, LOST_STOLEN, LITIGATION_HOLD, FREERIDING
+}
